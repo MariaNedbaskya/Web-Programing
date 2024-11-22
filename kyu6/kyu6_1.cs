98dccb4beadcc54710736b07924f8d64f417921d
MariaNedbaskya <mashanedbaskya@gmail.com>
2024-11-22 09:53:01 +0300

    Create kyu6_1.cs

---
+++ kyu6/kyu6_1.cs
@@ -0,0 +1,31 @@
+//Вам будет дано число, и вам нужно будет вернуть его как строку в развернутой форме . Например:
+  // 12 --> "10 + 2"
+   //45 --> "40 + 2"
+//70304 --> "70000 + 300 + 4"
+//ПРИМЕЧАНИЕ: Все числа будут целыми числами больше 0.
+using System;  
+using System.Text;  
+public class Kata  
+{  
+    public static string ExpandedForm(long number)  
+    {  
+        var numberString = number.ToString();  
+        var parts = new StringBuilder();  
+        
+        for (int i = 0; i < numberString.Length; i++)  
+        {  
+            int digit = numberString[i] - '0';
+            long positionValue = (long)Math.Pow(10, numberString.Length - 1 - i); 
+            
+            if (digit != 0) // Если цифра не ноль  
+            {  
+                if (parts.Length > 0)  
+                {  
+                    parts.Append(" + ");  
+                }  
+                parts.Append(digit * positionValue);  
+            }  
+        }  
+        return parts.ToString();  
+    }  
+}
