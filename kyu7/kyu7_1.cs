dc1f409652d4e12d47d132324657c7d0707c85c6
MariaNedbaskya <mashanedbaskya@gmail.com>
2024-11-22 10:28:14 +0300

    Update kyu7_1.cs

--- kyu7/kyu7_1.cs
+++ kyu7/kyu7_1.cs
@@ -1,6 +1,4 @@
-//Implement a function that takes two numbers m and n and returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
-//Ex.
-//(3, 5.0) --> [5.0, 10.0, 15.0]
+
 using System.Linq;
 public class Kata  
 {  
