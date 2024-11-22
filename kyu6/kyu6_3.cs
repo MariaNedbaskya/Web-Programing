01f9106d241dfc56e1d629d3cd834f121933c3b3
MariaNedbaskya <mashanedbaskya@gmail.com>
2024-11-22 10:43:42 +0300

    Create kyu6_3.cs

---
+++ kyu6/kyu6_3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace WebProg
+{
+    internal class Kyu6_3
+    {
+  public static int Add(int n)  
+    {  
+        if (n < 10)  
+        {  
+            return n;  
+        }  
+        int sum = 0;  
+        while (n > 0)  
+        {  
+            sum += n % 10; 
+            n /= 10;
+        }  
+        return Add(sum);  
+    }  
+    }
+}
